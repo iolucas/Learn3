6bafdc2f54ee0a6a208fa4815213db56b496cd31
Lucas V. Oliveira <lucas.o@live.com>
2015-11-17 11:31:02 -0200

    prog

---
+++ index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <link rel="stylesheet" href="style2.css"/>
+	</head>
+
+
+	<body>
+		<section>
+			<svg id="svgContainer"></svg>
+		</section>
+		<script src="jquery/jquery-2.1.4.min.js"></script>
+
+		<script type="text/javascript" src="d3/d3.v3.min.js"></script>
+		<script type="text/javascript" src="main2.js"></script>
+	</body>
+</html>
