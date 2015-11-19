e74f73d9824a83a77636cf8e83b369e92c5c0d2a
Lucas V. Oliveira <lucas.o@live.com>
2015-11-19 10:02:30 -0200

    prog

--- index.php
+++ index.php
@@ -3,30 +3,14 @@
 	<head>
 		<meta charset="utf-8">
 	    <link rel="stylesheet" href="style.css"/>
-	    <link rel="stylesheet" href="vex/vex.css" />
-		<link rel="stylesheet" href="vex/vex-theme-os.css" />
 	</head>
 
 
 	<body>
 		<section>
 			<svg id="svgContainer"></svg>
-			<div id="mainContent">
-				Lucas V. Oliveira<br><br>
-				Teste<br>
-				Teste<br>
-
-			</div>
-			<div id="loginSpace">
-
-
-			</div>
-
-			<div id="footDiv">Learn3</div>
 		</section>
 		<script src="jquery/jquery-2.1.4.min.js"></script>
-		<script src="vex/vex.combined.min.js"></script>
-		<script>vex.defaultOptions.className = 'vex-theme-os';</script>
 
 		<script type="text/javascript" src="d3/d3.v3.min.js"></script>
 		<script type="text/javascript" src="main.js"></script>
--- main.js
+++ main.js
@@ -1,241 +1,247 @@
+var width = 900;
+var height = 1800;
+
+
 var svgContainer = d3.select("#svgContainer")
-	.on("mousedown", function() {
-		d3.select(this).style("cursor", "move");		
+	.attr("height", width)
+	.attr("width", height)
+	.style("background-color", "#ddd");
+
+//var graphContainer = svgContainer.append("g");
+
+
+var drag = d3.behavior.drag()
+	.origin(function(d) { return d; })
+	.on("drag", function(d) {
+		d.x = d3.event.x < 1 ? 1 : d3.event.x;
+		d.y = d3.event.y < 1 ? 1 : d3.event.y;
+
+		d3.select(this).attr("transform", "translate(" + d.x + " " + d.y + ")");	
+	});
+
+
+var dataCollection = [
+	{
+		id: 0,
+		name: "Math",
+		color: "#55f",
+		x: 100,
+		y: 20,
+		links: [
+			{ targetId: 1 },
+			{ targetId: 2 }
+		]
+	},
+	{
+		id: 1,
+		name: "Portuguese",
+		color: "#5f5",
+		x: 300,
+		y: 120,
+		links: [
+			{ targetId: 2 }
+		]
+	},
+	{
+		id: 2,
+		name: "Science",
+		color: "#f55",
+		inputs: 1,
+		outputs: 1,
+		x: 500,
+		y: 220,
+		links: []
+	}
+];
+
+
+//Set outputs references
+dataCollection.forEach(function(node, index) {
+
+	node.links.forEach(function(link) {
+		link.targetNode = dataCollection[link.targetId];
+	});
+
+});
+
+
+
+
+var skillNode = svgContainer.selectAll(".skill-node").data(dataCollection).enter()
+	.append("g")
+	.attr("id", function(d) {
+		return "skill-node-" + d.id;
 	})
-	.on("mouseup", function() {
-		d3.select(this).style("cursor", "");		
+	.attr("class", "skill-node")
+	.attr("transform", function(d) {
+		if(d.x == undefined)
+			d.x = 100;
+		
+		if(d.y == undefined)
+			d.y = 100;
+
+		return "translate(" + d.x + " " + d.y +")";
+
+	}).call(drag);
+	
+var skillNodeContainer = skillNode.append("rect")
+	.attr("class", "skill-node-container")
+	.attr("height", function(d) {
+		//d.containerHeight = 12 * d.outputs.length + 28;
+	
+		d.containerHeight = 40;	//DEBUG
+
+		return d.containerHeight;
+	})
+	//.attr("rx", 0)
+	//.attr("ry", 0)
+	.attr("stroke-width", 2)
+	.attr("stroke", "#fff")
+	.attr("fill", function(d) {
+		return d.color;
 	});
 
-var graphContainer = svgContainer.append("g");
 
-//Must find way to fix bug of infinite drag positions
-var zoomBehavior = d3.behavior.zoom()
-	.scaleExtent([0.1, 1])
-	.on("zoom", function(z) {
-		graphContainer.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+skillNode.append("rect")
+	.attr("x", 1)
+	.attr("y", 1)
+	.attr("width", 28)
+	.attr("height", function(d) {
+		return d.containerHeight - 2;	
+	})
+	.attr("stroke", "none")
+	.attr("fill-opacity", 0.2)
+	.attr("fill", "#000");
+
+
+
+skillNode.append("text")
+	.attr("class", "skill-node-label")
+	.text(function(d) { return d.name; })
+	.attr("x", 38)
+	.attr("y", function(d) {
+		//Set y position in function of container height
+		
+		var textBox = this.getBBox();
+		
+		d.containerWidth = textBox.width < 40 ? 100 : textBox.width + 60;
+
+		return (d.containerHeight - textBox.height) / 2 - textBox.y;
 	});
 
-//Function to refresh the screen size
-var refreshScreenSize = function() {
-	svgContainer.attr("height", window.innerHeight)
-		.attr("width", window.innerWidth);
+//input symbol
+skillNode.append("rect")
+	.attr("width", 10)
+	.attr("height", 10)
+	.attr("fill", "#aaa")
+	.attr("rx", 2)
+	.attr("ry", 2)
+	.attr("stroke", "#fff")
+	.attr("stroke-width", 2)
+	.attr("x", -5)
+	.attr("y", function(d) {
+		return (d.containerHeight - 10) / 2;
+	});
 
-	zoomBehavior.size([window.innerWidth, window.innerHeight]);
-}
+//output symbol
+skillNode.append("rect")
+	.attr("width", 10)
+	.attr("height", 10)
+	.attr("fill", "#aaa")
+	.attr("rx", 2)
+	.attr("ry", 2)
+	.attr("stroke", "#fff")
+	.attr("stroke-width", 2)
+	.attr("x", function(d) {
+		return d.containerWidth - 5;
+	})
+	.attr("y", function(d) {
+		return (d.containerHeight - 10) / 2;
+	});
 
-window.addEventListener("resize", refreshScreenSize);
-window.addEventListener("load", refreshScreenSize);
-
-svgContainer.call(zoomBehavior);
+skillNodeContainer.attr("width", function(d) { return d.containerWidth; });
 
 
+//Create the links
 
-var width = window.innerWidth;
-var height = window.innerHeight;
+var createLink = d3.svg.diagonal()
+	.source(function(d) { return {"x":d.source.y, "y":d.source.x}; })            
+    .target(function(d) { return {"x":d.target.y, "y":d.target.x}; })
+    .projection(function(d) { return [d.y, d.x]; });
+
+
+
+//Iterate thru all the skill nodes
+skillNode.each(function(d) {
+	
+	if(d.links == undefined || d.links.length < 1)
+		return;
+
+	var linkSource = { x: d.x + d.containerWidth, y: d.y + 20 }
+
+	//Iterate thru the output links of the node
+	d.links.forEach(function(link) {
+
+		var linkTarget = { x: link.targetNode.x, y: link.targetNode.y + 20 }
+
+
+		var linkPath = createLink({ source: linkSource, target: linkTarget });
+
+
+		svgContainer.insert("path", ":first-child")
+			.attr("class", "skill-link")
+			.attr("d", linkPath)
+			.attr("fill", "none")
+			.attr("stroke", "#000")
+			.attr("stroke-width", 2);
+	});
+
+});
+
+
 
 
 
 
 //Open file and execute main function
-d3.csv("dataLinks.txt", dataOpened);
+//d3.csv("dataNodes.txt", dataOpened);
 
 
-function dataOpened(dataLinks) {
+function dataOpened(dataNodes) {
 
-	var dataNodes = {}
-
-	//Populate nodes with links
-	dataLinks.forEach(function(link) {
-
-		//Generate objects for nodes that doesn't have, and return the object ref to the link source/target
-		//link references are automatic done in case source/target are assigned with integers (index of the nodes)
-
-		link.source = dataNodes[link.source] || (dataNodes[link.source] = {name: link.source});
-  		link.target = dataNodes[link.target] || (dataNodes[link.target] = {name: link.target});
-	});
-
-	//Convert key value object to regular array
-	dataNodes = d3.values(dataNodes);
-
-	if(dataNodes.length > 0)
-		dataNodes[0].fixed = true;	
-
-
-	var links = graphContainer.selectAll("line").data(dataLinks).enter()
-		.append("line")
-  		.attr("stroke", "#222")
-  		.attr("stroke-width", 2);
-
-
-
-
-	var nodes = graphContainer.selectAll(".nodeCirc").data(dataNodes).enter()
-		.append("circle")
-		.style("cursor", "pointer")
-		.attr("class", "nodeCirc")
-		.attr("cx", function(d) {
-			if(d.fixed)
-				d.x = width / 2;
-			else
-				d.x = width / 2 + 10;
-
-			return d.x;
+	var nodes = graphContainer.selectAll(".node-rect").data(dataNodes).enter()
+		.append("g")
+		.attr("class", "node-rect")
+		.attr("transform", function(d) {
+			d.x = 0;
+			d.y = 0;
+			return "translate(0 0)";
 		})
-		.attr("cy", function(d) {
-			if(d.fixed)
-				d.y = height / 2;
-			else
-				d.y = height / 2 - 10;
+		.call(drag);
+
+	nodes.append("rect")
+		.attr("class", "node-rect")
+		.attr("fill", "#eee")
+		.attr("rx", "5")
+		.attr("stroke", "#222")
+		.attr("stroke-width", 2)
+		.attr("width", 120)
+		.attr("height", 40);
+
+	nodes.append("text")
+		.attr("fill", "#222")
+		.attr("font-size", 25)
+		.text(function(d) { return d.name; })
+		.attr("x", function(d) {
+			return (120 - this.getBBox().width) / 2;
+		})
+		.attr("y", function(d) {
+			var thisBox = this.getBBox(); 
+			return thisBox.height;
+		});
 			
-			return d.y;
-		})
-		.attr("r", function(d) {
-			if(d.fixed)
-				d.nodeRadius = 50;
-			else
-				d.nodeRadius = 20;
+	
 
-			return d.nodeRadius;
-		})
-		.attr("fill", function(d){
-			if(d.fixed)
-				return "yellow";
-
-			return "#222";
-			return "rgb(202,149,255)";
-		})	
-		.attr("stroke", "#0A091A")
-		.attr("stroke", "#fff")		
-		.attr("stroke-width", 5)
-		.attr("stroke-width", 3)
-		.on("mouseover", function (d) {
-			//d3.select(this).transition(100).attr("r", d.nodeRadius * 5);
-			d3.select(this).transition(200).attr("stroke", "#00a")
-				.attr("stroke-width", 5);
-		})
-		.on("mouseout", function (d) {
-			//d3.select(this).transition(100).attr("r", d.nodeRadius);
-			d3.select(this).transition(200).attr("stroke", "#fff")
-				.attr("stroke-width", 3);
-		})
-		.on("click", betaDialog);
-
-	var arrows = graphContainer.selectAll(".arrow").data(dataLinks).enter()
-  		.append("path")
-  		.attr("class", "arrow")
-  		.attr("fill", "#222")
-  		.attr("d", "M18,0 l20,5 v-10z");
-
-	var labels = graphContainer.selectAll("text").data(dataNodes).enter()
-		.append("text")
-    	.attr("fill", function(d){
-    		if(d.fixed)
-    			return "#fff";
-
-    		return "#fff";
-    	})
-    	.attr("font-size", 25)
-    	.attr("font-family", "Segoe UI")
-    	.style("text-shadow", "0 1px 0 #000, 1px 0 0 #000, 0 -1px 0 #000, -1px 0 0 #000")
-    	.text(function(d) { 
-    		if(d.name) 
-    			return d.name; 
-    		return "No Name";
-    	});
-
-
-	//Create force layout
-	var force = d3.layout.force()
-		.linkDistance(50)	//distance between two nodes
-		.linkStrength(1)	 //rigity of the link (how compressed or streched it will be once it is pulled/pushed)
-		.size([width, height])	//size of the force field space 
-		.charge(function(oi) {
-			if(oi.fixed)
-				return -15000;
-
-			return -2000;
-
-			return -1000;
-		})	//negative values means node repulsion, position means attraction
-		//.chargeDistance(100000)
-		.nodes(dataNodes)
-		.links(dataLinks)
-		.gravity(0.1)
-		.on("tick", updatePositions)
-		.on("end", updatePositions)
-		.alpha(0)
-		.start();
-
-		//nodes.call(force.drag);
-
-		function updatePositions(evt) {
-
-			/*if(evt.type == "end") {
-				nodes.attr("test", function(n){
-					n.fixed = true;
-				});
-
-				//force.gravity(0);
-
-				var containerBox = graphContainer[0][0].getBBox();
-
-				var scaleHeight = containerBox.height / height;
-				var scaleWidth = containerBox.width / width;
-
-				var contScale;
-
-				if(scaleHeight < scaleWidth) {
-					graphContainer.attr("transform", "scale(" + 0.2 + ")");
-				} else {
-					graphContainer.attr("transform", "scale(" + 0.2 + ")");
-				}
-
-
-				//console.log(dHeight);
-				//console.log(dWidth);
-			}*/
-
-			nodes.attr("cx", function(d) { return d.x; })
-				.attr("cy", function(d) { return d.y; });
-
-
-			links.attr("x1", function(d) { return d.source.x; })
-				.attr("y1", function(d) { return d.source.y; })
-				.attr("x2", function(d) { return d.target.x; })
-				.attr("y2", function(d) { return d.target.y; });
-
-
-			labels.attr("x", function(d) { 
-				if(d.fixed)
-					return d.x - 30; 
-
-				return d.x + 12
-			})
-    		.attr("y", function(d) { return d.y + 8; });
-
-    		arrows.attr("transform", function(d) {
-
-    			var invertAngle = (d.target.x > d.source.x) ? 180 : 0;
-
-    			var betaTan = (d.target.y - d.source.y) / (d.target.x - d.source.x);
-
-    			d.linkAngle = (180 / Math.PI) * Math.atan(betaTan) + invertAngle;
-
-    			var translateStr = "translate(" + d.target.x + " " + d.target.y + ")";
-    			var rotateStr = "rotate(" + d.linkAngle + ")";
-
-				return translateStr + " " + rotateStr;
-
-			});
-		}
 }
 
-function betaDialog() {
-	vex.dialog.alert('Thanks for checking out Vex!');
-	/*vex.dialog.confirm({
-		message: 'Are you absolutely sure you want to destroy the alien planet?',
-		callback: function(value) {
-			return console.log(value ? 'Successfully destroyed the planet.' : 'Chicken.');
-		}
-	});*/
-}
+
